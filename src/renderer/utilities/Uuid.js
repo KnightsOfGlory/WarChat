f6c57fa21b2d2e5444bb0f7ec3ba8a41284afe21
Richard Pianka <richardpianka@gmail.com>
2022-12-13 14:35:27 -0500

    fix(build): swapping out crypto for uuids with getrandomvalues

--- src/renderer/utilities/Uuid.js
+++ src/renderer/utilities/Uuid.js
@@ -1,5 +1,7 @@
+import getRandomValues from "get-random-values";
+
 export default function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        (c ^ getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
