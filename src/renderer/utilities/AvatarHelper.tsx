af91fee72a668cd575572319dd8bfb1693bc318f
Richard Pianka <richardpianka@gmail.com>
2022-12-14 12:19:21 -0500

    fix(users): allowing classic telnet to use boring avatars, resolves #72

--- src/renderer/utilities/AvatarHelper.tsx
+++ src/renderer/utilities/AvatarHelper.tsx
@@ -4,25 +4,28 @@ import Avatar from "@mui/material/Avatar"
 import BoringAvatar from "boring-avatars"
 import {ProductIcons} from "./ProductIcons";
 import {UserFlags} from "./UserFlags";
+import {ProfileManager} from "../state/ProfileManager";
 
 export namespace AvatarHelper {
 
     export function getAvatar(user: User): ReactElement {
         let icon = ProductIcons.getByClient(user.client.trim(), user.flags as string)
+        let special =
+            ProfileManager.getProfile().init6 ?
+                UserFlags.Init6.isAdministrator(user.flags) ||
+                UserFlags.Init6.isOperator(user.flags)
+            :
+                UserFlags.isAdministrator(user.flags) ||
+                UserFlags.isOperator(user.flags)
         let useBoring =
             !user.bot &&
             user.client == "[CHAT]" &&
-            !UserFlags.Init6.isAdministrator(user.flags) &&
-            !UserFlags.Init6.isOperator(user.flags)
+            !special
 
         return useBoring ? (
             <BoringAvatar
                 name={user.name}
                 variant="beam"
-                // colors={["#F8F8D6", "#B3C67F", "#5D7E62", "#50595C", "#FA3E3E"]}
-                // colors={["#1976D2", "#009DE6", "#00BED8", "#8DEE86", "#FA3E3E", "#F9F871"]}
-
-                // colors={["#1976D2", "#92A9DE", "#E3F0FF", "#D8A31A"]}
                 colors={["#1976D2", "#414756", "#A5ABBD", "#C94669"]}
             />
         ) : (
