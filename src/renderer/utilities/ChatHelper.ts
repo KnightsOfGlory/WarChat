c3858f08b0ef25193cf942253667365ee300f470
Richard Pianka <richardpianka@gmail.com>
2022-12-20 22:25:49 -0500

    fix(chat): fixing channel message parsing bug

--- src/renderer/utilities/ChatHelper.ts
+++ src/renderer/utilities/ChatHelper.ts
@@ -28,6 +28,7 @@ export namespace ChatHelper {
         "Apathy3 - Unstable and damn near unusable",
         "Apathy2 - Unstable and damn near unusable",
         "Its Hammer Time :) - DC v1.2.",
+        "Its Hammer Time :) - DC v1.3b1 [Private Edition]",
         ":+:~EwR 4 LyFe~:+: - Ghost 3.02",
         "-[ +|{W+ ]-[ Subaru Version 1.3.5 MoonGlade Series ]-"
     ]
