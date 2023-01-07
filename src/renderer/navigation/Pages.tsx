3117ce29807b10d1b529738e34a4c7748c486daf
Richard Pianka <richardpianka@gmail.com>
2023-01-06 23:23:06 -0500

    feat(chat): making the whisper tab a toggle setting

--- src/renderer/navigation/Pages.tsx
+++ src/renderer/navigation/Pages.tsx
@@ -11,6 +11,8 @@ import {useEffect, useState} from "react";
 import {References} from "@knightsofglory/warlibrary/lib/References";
 import TodayIcon from '@mui/icons-material/Today';
 import LeakAddIcon from '@mui/icons-material/LeakAdd';
+import {Settings} from "@knightsofglory/warlibrary/lib/state/SettingsManager";
+import {Messages} from "@knightsofglory/warlibrary/lib/common/Messages";
 
 type Properties = {
     page: number,
@@ -20,6 +22,7 @@ type Properties = {
 export default function Pages(properties: Properties) {
 
     const [unread, setUnread] = useState(0)
+    const [whisperTab, setWhisperTab] = useState(References.settingsManager.getSettings().whisperTab)
 
     useEffect(() => {
         References.messageBus.onLocal(
@@ -27,6 +30,17 @@ export default function Pages(properties: Properties) {
             (count) => {
                 setUnread(count as number)
             }
+        )
+        References.settingsManager.subscribe(
+            "settings",
+            (settings: Settings) => {
+                console.log(settings.whisperTab)
+                setWhisperTab(settings.whisperTab)
+            }
+        )
+        References.messageBus.send(
+            Messages.Channels.SETTINGS,
+            Messages.Commands.Settings.READ
         )
     }, [])
 
@@ -49,7 +63,11 @@ export default function Pages(properties: Properties) {
             >
                 <Tab icon={<ChatBubbleIcon/>} label="CHANNEL" sx={padding}/>
                 <Tab icon={<GroupsIcon/>} label="FRIENDS" sx={padding}/>
-                <Tab icon={<Badge badgeContent={unread} color={"error"}><ForumIcon/></Badge>} label="WHISPERS" sx={padding}/>
+                {
+                    whisperTab ?
+                    <Tab icon={<Badge badgeContent={unread} color={"error"}><ForumIcon/></Badge>} label="WHISPERS" sx={padding}/> :
+                    null
+                }
                 <Divider/>
                 <Tab icon={<TodayIcon/>} label="MOTD" sx={padding}/>
                 <Tab icon={<LeakAddIcon/>} label="DATA" sx={padding}/>
